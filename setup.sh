3826798180f26c89d9b49b11164f4c8c723bd808
Sukwon Choi <swchoi06@snatcher.ai>
2019-01-21 15:39:01 +0900

    Added .flake config file

--- setup.sh
+++ setup.sh
@@ -7,9 +7,19 @@ BASE_DIR=$(dirname $0)
 cd $BASE_DIR
 git submodule init
 git submodule update
+
+if [ -f ~/.vimrc ]; then
+  mv ~/.vimrc ~/.vimrc_backup
+fi
+if [ -f ~/.config/.flake ]; then
+  mv ~/.config/.flake ~/.config/.flake_old
+fi
+
 rm -rf ~/.vimrc
 rm -rf ~/.vim/
+rm -rf ~/.config/.flake
 cp -R .vim ~/
 cp -R .vimrc ~/
+cp -R .flake ~/.config/
 
 cd $CURRENT_DIR
